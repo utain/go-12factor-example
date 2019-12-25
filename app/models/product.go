1168262d0183944806bb37b337a7114ecbd5b394
utain <utainwp@gmail.com>
2019-12-25 10:21:05 +0700

    rm log

--- app/models/product.go
+++ app/models/product.go
@@ -3,21 +3,8 @@ package models
 import (
 	"database/sql/driver"
 	"encoding/json"
-	"os"
-
-	"github.com/sirupsen/logrus"
+	"fmt"
 )
-
-var log = logrus.New()
-
-func init() {
-	log.Out = os.Stdout
-	log.SetLevel(logrus.DebugLevel)
-	log.WithFields(logrus.Fields{
-		"animal": "walrus",
-		"size":   10,
-	}).Info("A group of walrus emerges from the ocean")
-}
 
 // Product model
 type Product struct {
@@ -34,14 +21,14 @@ type AttrType map[string]string
 
 // Value convert golang datatype (struct)  to json-string (sql datatype) for save to database.
 func (attr AttrType) Value() (driver.Value, error) {
-	log.Debug("Value::", attr)
+	fmt.Println("Value::", attr)
 	attrVl, err := json.Marshal(attr)
 	return string(attrVl), err
 }
 
 //Scan convert database value (sql datatype) to golang datatype (struct).
 func (attr *AttrType) Scan(src interface{}) error {
-	log.Debug("Scane:", src)
+	fmt.Println("Scane:", src)
 	err := json.Unmarshal([]byte(src.(string)), &attr)
 	return err
 }
