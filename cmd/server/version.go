15d985486256f8a580860af0996d3c3925533727
utain <utainwp@gmail.com>
2020-01-02 01:59:29 +0700

    inject version (var) at build time

--- cmd/server/version.go
+++ cmd/server/version.go
@@ -4,13 +4,21 @@ import "github.com/spf13/cobra"
 
 import "fmt"
 
+// GitCommit inject by -ldflags
+// GIT_COMMIT=git rev-list -1 HEAD && go build -ldflags "-X main.GitCommit=$GIT_COMMIT"
+var GitCommit string
+
+// Version of module from go.mod
+var Version string
+
 var (
 	versionCMD = &cobra.Command{
 		Use:   "version",
 		Short: "check version of command",
 		Long:  `check version of command`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Go Example V1.0.0")
+			fmt.Println("Server Version:", Version)
+			fmt.Println("Git Commit:", GitCommit)
 		},
 	}
 )
